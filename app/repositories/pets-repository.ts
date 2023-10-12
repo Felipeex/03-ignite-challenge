78d2abc6b7019465ac3410a1b7f0667833b5c988
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-10-12 01:47:07 -0300

    feat: create pets contract.

--- app/repositories/pets-repository.ts
+++ app/repositories/pets-repository.ts
@@ -1,1 +1,6 @@
-interface PetRepository {}
+import { Pet, Prisma } from "@prisma/client";
+
+export interface PetRepository {
+  getById: (id: string) => Promise<Pet | null>;
+  register: (data: Prisma.PetUncheckedCreateInput) => Promise<Pet>;
+}
