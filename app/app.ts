87530d03edecf30458112fee402d960a16bc9f25
Felipe Lima <70120728+Felipeex@users.noreply.github.com>
2023-10-28 03:21:35 -0300

    feat: create error handler.

--- app/app.ts
+++ app/app.ts
@@ -1,2 +1,21 @@
 import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './environment'
+import { petsRoutes } from './infra/http/controllers/pets/routes'
 export const app = fastify()
+
+app.register(petsRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
